27fab6fc8f0b21da7394aabbfff4a592b4d16ceb
lliillllliililliilii <lazulite333@gmail.com>
2025-12-29 23:59:14 +0900

    2
    added guides
    fixed miscs

--- JS/mainScript.js
+++ JS/mainScript.js
@@ -1,7 +1,7 @@
 // 현재 페이지의 파일명(예: about.html)을 가져옵니다.
 const currentPage = window.location.pathname.split("/").pop();
 
-// 내비게이션 바의 모든 링크를 검사합니다.
+// 내비게이션 바의 모든 링크를 검사
 document.querySelectorAll('.nav-bar a').forEach(link => {
     // 링크의 href 속성값이 현재 페이지 파일명과 같다면
     if (link.getAttribute('href') === currentPage) {
@@ -12,4 +12,46 @@ document.querySelectorAll('.nav-bar a').forEach(link => {
     if (currentPage === "" || currentPage === "index.html") {
         if (link.textContent === "Home") link.classList.add('active');
     }
+});
+
+
+// 랜덤 링크 연결
+
+// 링크 목록
+const linkPool = [
+    //미러시
+    "https://youtu.be/f3PC1VwJsis?si=wrCGCb9hvEeckjQc",
+    //미러시2
+    "https://youtu.be/Tq3ONIeDglY?si=zRbcVdwn9c1Z4E6P&t=42",
+    //미러시 쇼츠
+    "https://youtube.com/shorts/Q2yHBY6j_Ok?si=54VKCKi8YBkPbqQP",
+
+    // 림버스 스토리 요약
+    "https://youtu.be/KOpRyDa7LaA?si=y7MpjcF2g2tATrj0",
+
+    //아스고어 밈
+    "https://youtu.be/u5NqO2v_xnY?si=nymTLknynyK_LZJE",
+
+    // Oiia 고양이 + 케데헌 골든
+    "https://youtu.be/IlK42_MFmo4?si=Jm07WZiFKrFwyL6B",
+    // 소다팝
+    "https://youtu.be/ZbCVS_OgPIc?si=jaOvj4Iy3M9tRmXk",
+
+
+    //산나비 마리 테마
+    "https://youtu.be/5bcC7jtqSkI?si=zev4YrGljjt9S1PH"
+    //라오루 테마2
+    ,"https://music.youtube.com/watch?v=dn67NYqrtgQ&si=WQ9cR0Tue0yO11R_"
+];
+
+// 클래스가 "random-link"인 요소를 찾아서 이벤트를 걸어줌
+document.querySelector('.random-link').addEventListener('click', function(event) {
+    event.preventDefault(); // 기본 이동을 막고
+
+    // 배열에서 랜덤하게 하나를 뽑음
+    const randomIndex = Math.floor(Math.random() * linkPool.length);
+    const selectedLink = linkPool[randomIndex];
+
+    // 선택된 페이지로 이동
+    window.open(selectedLink, '_blank');
 });
